6cc9a0fef7caee2aa41483b7c7aeba4c2bd61e03
Adibas03 <adibas03@ymail.com>
2017-10-24 21:01:10 +0100

    Add contract to use StandardToken

---
+++ contracts/standardTokenContract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.11;
+
+import './StandardToken.sol';
+
+
+contract standardTokenContract is StandardToken{
+
+  //Required to Instantiate nummber of tokens and give creator all the tokens
+  function standardTokenContract(){
+    totalSupply = 100000000;
+    balances[msg.sender] = totalSupply;
+  }
+
+
+}
