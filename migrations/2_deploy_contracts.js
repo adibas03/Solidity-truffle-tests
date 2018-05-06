6e727543fe8de24fd9a17c8d18a7eb8f641adc0f
Adibas03 <adibas03@ymail.com>
2018-05-06 11:42:41 +0100

    update deployer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,11 @@ var safeMath = artifacts.require('./safeMathContract.sol');
 var SafeMath = artifacts.require('./SafeMath.sol');
 var Owned = artifacts.require('./ownedContract.sol');
 var Ownable = artifacts.require('./Ownable.sol');
+var StandardToken = artifacts.require('./StandardToken.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
-  deployer.link(SafeMath,[safeMath] );
+  deployer.link(SafeMath,[safeMath,StandardToken] );
   deployer.deploy(Ownable);
   deployer.link(Ownable,[Owned] );
 };
